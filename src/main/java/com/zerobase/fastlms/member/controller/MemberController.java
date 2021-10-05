84c080c5a25546bee1a2a544c19bb1b10f1428de
noah0504789 <noah0504@naver.com>
2021-10-05 23:11:26 +0900

    회원가입 + DB

---
+++ src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
@@ -0,0 +1,38 @@
+package com.zerobase.fastlms.member.controller;
+
+import com.zerobase.fastlms.member.entity.Member;
+import com.zerobase.fastlms.member.model.MemberInput;
+import com.zerobase.fastlms.member.repository.MemberRepository;
+import com.zerobase.fastlms.member.service.MemberServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+
+@Controller
+@AllArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+    private final MemberServiceImpl memberService;
+
+    @GetMapping(value = "/member/register")
+    public String register() {
+        return "member/register";
+    }
+
+    @PostMapping(value = "/member/register")
+    public String registerSubmit(Model model, HttpServletRequest request, HttpServletResponse response, MemberInput parameter) {
+
+        boolean result = memberService.register(parameter);
+        model.addAttribute("result", result);
+
+        return "member/register_complate";
+    }
+
+}
