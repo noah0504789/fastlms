8e96a809e01dbb1ef8fc51f95e0f3ec3c17ef2e1
noah0504789 <noah0504@naver.com>
2021-10-05 21:40:56 +0900

    response 객체로 html content 제공 및 index.html

--- src/main/java/com/zerobase/fastlms/MainPage.java
+++ src/main/java/com/zerobase/fastlms/MainPage.java
@@ -1,21 +1,32 @@
 package com.zerobase.fastlms;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
 public class MainPage {
 
     @RequestMapping("/")
     public String index() {
-        return "Index page";
+        return "index";
     }
 
     @RequestMapping("/hello")
-    public String hello() {
-        return "hello";
+    public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String msg = "hello";
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter printWriter = response.getWriter();
+
+        printWriter.write(msg);
+        printWriter.close();
+
     }
-
-
-
 }
